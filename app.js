0315dda00dc107294dfd750f64445aaa9c6ea9aa
Kosminski1 <jim.kaminski.96@gmail.com>
2024-01-29 00:08:13 +0100

    bugfixing

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
             matchedPairs++;
 
             if (matchedPairs === totalPairs) {
-               // createConfetti();
-                alert(`Congratulations! You won the in ${attempts} attempts!`);
+                createConfetti();
+                //alert(`Congratulations! You won the in ${attempts} attempts!`);
             }
 
         } else {
@@ -96,9 +96,24 @@ document.addEventListener('DOMContentLoaded', () => {
             const confetti = document.createElement('div');
             confetti.className = 'confetti';
             confetti.style.left = Math.random() * 100 + 'vw';
+            confetti.style.backgroundColor = getRandomColor();
             confetti.style.animationDuration = Math.random() * 2 + 1 + 's';
             confettiContainer.appendChild(confetti);
         }
+
+    // Remove confetti after 10 seconds
+    setTimeout(() => {
+        confettiContainer.innerHTML = '';
+    }, 10000);
+    }
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
 
     // Function to toggle dark mode
